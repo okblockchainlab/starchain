375c8de413e02731f134db2594af0471182bc2eb
fatcat22 <pandayt@163.com>
2018-09-02 22:20:07 +0800

    createrawtransaction/signrawtransaction success

--- okwallet/okwallet/wallet.go
+++ okwallet/okwallet/wallet.go
@@ -4,12 +4,21 @@ import (
 	"bytes"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"log"
+	"math/rand"
 	"os"
+	"strconv"
 
 	"starchain/account"
+	"starchain/common"
 	"starchain/common/config"
+	"starchain/core/contract"
+	"starchain/core/signature"
+	"starchain/core/transaction"
 	"starchain/crypto"
+	stc_errors "starchain/errors"
+	"starchain/util"
 )
 
 func init() {
@@ -52,8 +61,8 @@ func init() {
       "Transfer": 0.000
     }
   }
-}
-`)
+}`)
+
 	file = bytes.TrimPrefix(file, []byte("\xef\xbb\xbf"))
 	cfg := config.ConfigFile{}
 	err := json.Unmarshal(file, &cfg)
@@ -68,16 +77,357 @@ func init() {
 	crypto.SetAlg(config.Parameters.EncryptAlg)
 }
 
-func GetAddressByPrivateKey(prvkeyStr string) (string, error) {
+func GetAddressByPrivateKey(prvkeyStr string) (string, string, error) {
+	prvkey, err := hex.DecodeString(prvkeyStr)
+	if err != nil {
+		return "", "", err
+	}
+
+	account, err := account.NewAccountWithPrivatekey(prvkey)
+	if err != nil {
+		return "", "", err
+	}
+	pubkey, err := account.PubKey().EncodePoint(true)
+	if err != nil {
+		return "", "", err
+	}
+
+	addrStr, err := account.ProgramHash.ToAddress()
+
+	return common.BytesToHexString(pubkey), addrStr, err
+}
+
+type UTXOUnspentInfo struct {
+	Txid  string
+	Index uint32
+	Value string
+}
+
+type OKTXOutput struct {
+	AssetID     string
+	Value       int64
+	ProgramHash string
+}
+
+type InputTransaction struct {
+	Txid           string
+	UnSpendOutputs []OKTXOutput `json:"outputs"`
+}
+
+type RawTxInfo struct {
+	AssetID      string
+	Pubkey       string
+	FeePerTarget string `json:"fee_per_target"`
+	Targets      []util.BatchOut
+	UnspentInfo  []UTXOUnspentInfo  `json:"unspent_info"`
+	InputTx      []InputTransaction `json:"input_tx"`
+}
+
+type RawTransaction struct {
+	TxData  string
+	InputTx []InputTransaction `json:"input_tx"`
+}
+
+//OKMockLedgerStore is used for
+//transaction.TxStore.GetTransaction called in transaction.GetReference
+type OKMockLedgerStore struct {
+	InputTx map[common.Uint256][]*transaction.TxOutput
+}
+
+func (l *OKMockLedgerStore) GetTransaction(hash common.Uint256) (*transaction.Transaction, error) {
+	o, ok := l.InputTx[hash]
+	if !ok {
+		return nil, errors.New("can not find transaction")
+	}
+
+	return &transaction.Transaction{Outputs: o}, nil
+}
+
+func (l *OKMockLedgerStore) GetQuantityIssued(AssetId common.Uint256) (common.Fixed64, error) {
+	return common.Fixed64(0), errors.New("not implemented")
+}
+
+func makeOKMockLedgerStore(InputTx []InputTransaction) (*OKMockLedgerStore, error) {
+	result := OKMockLedgerStore{make(map[common.Uint256][]*transaction.TxOutput)}
+
+	for _, it := range InputTx {
+		txidBytes, err := hex.DecodeString(it.Txid)
+		if err != nil {
+			return nil, err
+		}
+		refID, err := common.Uint256ParseFromBytes(txidBytes)
+		if err != nil {
+			return nil, err
+		}
+
+		var outputs []*transaction.TxOutput
+		for _, uo := range it.UnSpendOutputs {
+			assetid, err := string2AssetID(uo.AssetID)
+			if err != nil {
+				return nil, err
+			}
+
+			value := common.Fixed64(uo.Value)
+
+			phashBytes, err := hex.DecodeString(uo.ProgramHash)
+			if err != nil {
+				return nil, err
+			}
+			programhash, err := common.Uint160ParseFromBytes(phashBytes)
+			if err != nil {
+				return nil, err
+			}
+
+			outputs = append(outputs, &transaction.TxOutput{AssetID: assetid, Value: value, ProgramHash: programhash})
+		}
+
+		result.InputTx[refID] = outputs
+	}
+
+	return &result, nil
+}
+
+//OKSigner is used for signature.SignBySigner
+type OKSigner []byte
+
+func (s OKSigner) PrivKey() []byte {
+	return []byte(s)
+}
+
+func (s OKSigner) PubKey() *crypto.PubKey {
+	return crypto.NewPubKey([]byte(s))
+}
+
+func string2AssetID(s string) (common.Uint256, error) {
+	tmp, err := common.HexStringToBytesReverse(s)
+	if err != nil {
+		return common.Uint256{}, err
+	}
+
+	var assetID common.Uint256
+	if err := assetID.Deserialize(bytes.NewReader(tmp)); err != nil {
+		return common.Uint256{}, err
+	}
+
+	return assetID, nil
+}
+
+func programHashFromPubkey(pubkeyStr string) (common.Uint160, error) {
+	pubkeyBytes, err := hex.DecodeString(pubkeyStr)
+	if err != nil {
+		return common.Uint160{}, err
+	}
+
+	pubkey, err := crypto.DecodePoint(pubkeyBytes)
+	if err != nil {
+		return common.Uint160{}, err
+	}
+
+	signatureRedeemScript, err := contract.CreateSignatureRedeemScript(pubkey)
+	if err != nil {
+		return common.Uint160{}, stc_errors.NewDetailErr(err, stc_errors.ErrNoCode, "CreateSignatureRedeemScript failed")
+	}
+	programHash, err := common.ToCodeHash(signatureRedeemScript)
+	if err != nil {
+		return common.Uint160{}, stc_errors.NewDetailErr(err, stc_errors.ErrNoCode, "ToCodeHash failed")
+	}
+
+	return programHash, nil
+}
+
+func makeTransferTransaction(pubkey string, assetID common.Uint256, unspentInfo []UTXOUnspentInfo, feePerTargetStr string, batchOut ...util.BatchOut) (*transaction.Transaction, error) {
+	//TODO: check if being transferred asset is System Token(STC)
+	outputNum := len(batchOut)
+	if outputNum == 0 {
+		return nil, errors.New("nil outputs")
+	}
+
+	// get main account which is used to receive changes
+	programHash, err := programHashFromPubkey(pubkey)
+	if err != nil {
+		return nil, err
+	}
+
+	feePerTarget, err := common.StringToFixed64(feePerTargetStr)
+	if err != nil {
+		return nil, err
+	}
+
+	var expected common.Fixed64
+	input := []*transaction.UTXOTxInput{}
+	output := []*transaction.TxOutput{}
+	// construct transaction outputs
+	for _, o := range batchOut {
+		outputValue, err := common.StringToFixed64(o.Value)
+		if err != nil {
+			return nil, err
+		}
+		if outputValue <= feePerTarget {
+			return nil, errors.New("token is not enough for transaction fee")
+		}
+		expected += outputValue
+		address, err := common.ToScriptHash(o.Address)
+		if err != nil {
+			return nil, errors.New("invalid address")
+		}
+		tmp := &transaction.TxOutput{
+			AssetID:     assetID,
+			Value:       outputValue - feePerTarget,
+			ProgramHash: address,
+		}
+		output = append(output, tmp)
+	}
+
+	// construct transaction inputs and changes
+	for _, info := range unspentInfo {
+		value, err := common.StringToFixed64(info.Value)
+		if err != nil {
+			return nil, err
+		}
+
+		txidBytes, err := hex.DecodeString(info.Txid)
+		if err != nil {
+			return nil, err
+		}
+		refID, err := common.Uint256ParseFromBytes(txidBytes)
+		if err != nil {
+			return nil, err
+		}
+		inp := &transaction.UTXOTxInput{
+			ReferTxID:          refID,
+			ReferTxOutputIndex: uint16(info.Index),
+		}
+
+		input = append(input, inp)
+
+		if value > expected {
+			changes := &transaction.TxOutput{
+				AssetID:     assetID,
+				Value:       value - expected,
+				ProgramHash: programHash,
+			}
+			// if any, the changes output of transaction will be the last one
+			output = append(output, changes)
+			expected = 0
+			break
+		} else if value == expected {
+			expected = 0
+			break
+		} else if value < expected {
+			expected = expected - value
+		}
+	}
+
+	if expected > 0 {
+		return nil, errors.New("token is not enough")
+	}
+
+	// construct transaction
+	txn, err := transaction.NewTransferAssetTransaction(input, output)
+	if err != nil {
+		return nil, err
+	}
+	txAttr := transaction.NewTxAttribute(transaction.Nonce, []byte(strconv.FormatInt(rand.Int63(), 10)))
+	txn.Attributes = make([]*transaction.TxAttribute, 0)
+	txn.Attributes = append(txn.Attributes, &txAttr)
+
+	return txn, nil
+}
+
+func sign(signer signature.Signer, context *contract.ContractContext) error {
+
+	sig_contract, err := contract.CreateSignatureContract(signer.PubKey())
+	if err != nil {
+		return err
+	}
+
+	for _, hash := range context.ProgramHashes {
+		if hash != sig_contract.ProgramHash {
+			return errors.New("ProgramHash is invalid")
+		}
+
+		switch {
+		case sig_contract.IsStandard():
+			sig, err := signature.SignBySigner(context.Data, signer)
+			if err != nil {
+				return err
+			}
+			if err := context.AddContract(sig_contract, signer.PubKey(), sig); err != nil {
+				return err
+			}
+		default:
+			return errors.New("unknown contract type")
+		}
+	}
+
+	return nil
+}
+
+//reference: SendToManyAddress
+func CreateRawTransaction(infoStr string) (string, error) {
+	var createInfo RawTxInfo
+	if err := json.Unmarshal([]byte(infoStr), &createInfo); err != nil {
+		return "", err
+	}
+
+	assetID, err := string2AssetID(createInfo.AssetID)
+	if err != nil {
+		return "", err
+	}
+
+	txn, err := makeTransferTransaction(createInfo.Pubkey, assetID, createInfo.UnspentInfo, createInfo.FeePerTarget, createInfo.Targets...)
+	if err != nil {
+		return "", err
+	}
+
+	var buffer bytes.Buffer
+	txn.Serialize(&buffer)
+	txData := common.BytesToHexString(buffer.Bytes())
+
+	rawtx := RawTransaction{
+		TxData:  txData,
+		InputTx: createInfo.InputTx,
+	}
+
+	result, err := json.Marshal(&rawtx)
+	return string(result), err
+}
+
+//reference: begin of sendRawTransaction, and end of MakeTransferTransaction
+func SignRawTransaction(prvkeyStr, rawTxStr string) (string, error) {
 	prvkey, err := hex.DecodeString(prvkeyStr)
 	if err != nil {
 		return "", err
 	}
 
-	account, err := account.NewAccountWithPrivatekey(prvkey)
+	var rawtx RawTransaction
+	if err := json.Unmarshal([]byte(rawTxStr), &rawtx); err != nil {
+		return "", err
+	}
+
+	//copy from sendRawTransaction
+	rawtxn, _ := common.HexStringToBytes(rawtx.TxData)
+	var txn transaction.Transaction
+	if err := txn.Deserialize(bytes.NewReader(rawtxn)); err != nil {
+		return "", errors.New("invalid raw transaction")
+	}
+
+	signer := OKSigner(prvkey)
+
+	oldTxStore := transaction.TxStore
+	defer func() { transaction.TxStore = oldTxStore }()
+	transaction.TxStore, err = makeOKMockLedgerStore(rawtx.InputTx)
 	if err != nil {
 		return "", err
 	}
 
-	return account.ProgramHash.ToAddress()
+	ctx := contract.NewContractContext(&txn)
+	if err := sign(signer, ctx); err != nil {
+		return "", err
+	}
+	txn.SetPrograms(ctx.GetPrograms())
+
+	var buffer bytes.Buffer
+	txn.Serialize(&buffer)
+	return common.BytesToHexString(buffer.Bytes()), nil
 }
--- okwallet/okwallet/wallet_test.go
+++ okwallet/okwallet/wallet_test.go
@@ -6,13 +6,88 @@ import (
 
 func TestGetAddressByPrivateKey(t *testing.T) {
 	const prvkey = `de47f09131a701b1012ab205df484ec43dd73daa6d42a0fead004a16307d7fdd`
-	const expect = `ShoPrXXG2nMATE3WDA7B7sgJscCoNXauaY`
+	const expect_pub = `036f9c4bc2c75334da4c145848769c6128dd7edafa8059f28da5feb60e3d17e3df`
+	const expect_address = `ShoPrXXG2nMATE3WDA7B7sgJscCoNXauaY`
 
-	addr, err := GetAddressByPrivateKey(prvkey)
+	pub, addr, err := GetAddressByPrivateKey(prvkey)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if expect != addr {
-		t.Fatal("GetAddressByPrivateKey failed. expect " + expect + " but return " + addr)
+	if expect_pub != pub {
+		t.Fatal("GetAddressByPrivateKey failed. expect pubkey " + expect_pub + " but return " + pub)
+	}
+	if expect_address != addr {
+		t.Fatal("GetAddressByPrivateKey failed. expect address " + expect_address + " but return " + addr)
+	}
+}
+
+func TestCreateRawTransaction(t *testing.T) {
+	const input = `
+	{
+"assetid":"23d4d5aeb154126332cc9aa5dd0a4ce3bb4cf3d2df507bd9a85ab6b7b9c9bbf4",
+"pubkey":"036f9c4bc2c75334da4c145848769c6128dd7edafa8059f28da5feb60e3d17e3df",
+"fee_per_target": "1",
+"targets": [
+  {
+    "address":"SfgWjUW6H1NvFUgTBhrdxs6uf6upy2Tc5G",
+    "value":"100"
+  },
+  {
+    "address":"SbH2nd8p7veM2LG6uJsu6dDRnmww7B94ai",
+    "value":"200"
+  }
+],
+"unspent_info": [
+  {
+    "txid":"1da2fcb9a6487c7df825dd7324cacc4ac5b5c4d0e22d82594bc5365e0b7b9dcc",
+    "index": 0,
+    "value": "150"
+  },
+	{
+    "txid":"1da2fcb9a6487c7df825dd7324cacc4ac5b5c4d0e22d82594bc5365e0b7b9dcc",
+    "index": 1,
+    "value": "200"
+  }
+],
+"input_tx": [
+  {
+		"txid":"1da2fcb9a6487c7df825dd7324cacc4ac5b5c4d0e22d82594bc5365e0b7b9dcc",
+		"outputs": [
+			{
+				"assetid":"23d4d5aeb154126332cc9aa5dd0a4ce3bb4cf3d2df507bd9a85ab6b7b9c9bbf4",
+				"value":150,
+				"programhash":"e0f1a129f50fc717fe746c5d041d0054874c8609"
+			},
+			{
+				"assetid":"23d4d5aeb154126332cc9aa5dd0a4ce3bb4cf3d2df507bd9a85ab6b7b9c9bbf4",
+				"value":200,
+				"programhash":"e0f1a129f50fc717fe746c5d041d0054874c8609"
+			}
+		]
+	}
+]
+}
+	`
+
+	const expect = `{"TxData":"800001001335353737303036373931393437373739343130021da2fcb9a6487c7df825dd7324cacc4ac5b5c4d0e22d82594bc5365e0b7b9dcc00001da2fcb9a6487c7df825dd7324cacc4ac5b5c4d0e22d82594bc5365e0b7b9dcc010003f4bbc9b9b7b65aa8d97b50dfd2f34cbbe34c0adda59acc32631254b1aed5d4230003164e02000000c9b43543291886dfa20961bdea57cf0f0f945dcdf4bbc9b9b7b65aa8d97b50dfd2f34cbbe34c0adda59acc32631254b1aed5d42300e721a2040000009962cf555c2b9b0a142cfcec1787b1b534863096f4bbc9b9b7b65aa8d97b50dfd2f34cbbe34c0adda59acc32631254b1aed5d42300f2052a01000000e0f1a129f50fc717fe746c5d041d0054874c860900","input_tx":[{"Txid":"1da2fcb9a6487c7df825dd7324cacc4ac5b5c4d0e22d82594bc5365e0b7b9dcc","outputs":[{"AssetID":"23d4d5aeb154126332cc9aa5dd0a4ce3bb4cf3d2df507bd9a85ab6b7b9c9bbf4","Value":150,"ProgramHash":"e0f1a129f50fc717fe746c5d041d0054874c8609"},{"AssetID":"23d4d5aeb154126332cc9aa5dd0a4ce3bb4cf3d2df507bd9a85ab6b7b9c9bbf4","Value":200,"ProgramHash":"e0f1a129f50fc717fe746c5d041d0054874c8609"}]}]}`
+
+	rawTx, err := CreateRawTransaction(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expect != rawTx {
+		t.Fatal("CreateRawTransaction failed. expect result is " + expect + " but return " + rawTx)
+	}
+}
+
+func TestSignRawTransaction(t *testing.T) {
+	const prvkey = `de47f09131a701b1012ab205df484ec43dd73daa6d42a0fead004a16307d7fdd`
+	const input = `{"TxData":"800001001335353737303036373931393437373739343130021da2fcb9a6487c7df825dd7324cacc4ac5b5c4d0e22d82594bc5365e0b7b9dcc00001da2fcb9a6487c7df825dd7324cacc4ac5b5c4d0e22d82594bc5365e0b7b9dcc010003f4bbc9b9b7b65aa8d97b50dfd2f34cbbe34c0adda59acc32631254b1aed5d4230003164e02000000c9b43543291886dfa20961bdea57cf0f0f945dcdf4bbc9b9b7b65aa8d97b50dfd2f34cbbe34c0adda59acc32631254b1aed5d42300e721a2040000009962cf555c2b9b0a142cfcec1787b1b534863096f4bbc9b9b7b65aa8d97b50dfd2f34cbbe34c0adda59acc32631254b1aed5d42300f2052a01000000e0f1a129f50fc717fe746c5d041d0054874c860900","input_tx":[{"Txid":"1da2fcb9a6487c7df825dd7324cacc4ac5b5c4d0e22d82594bc5365e0b7b9dcc","outputs":[{"AssetID":"23d4d5aeb154126332cc9aa5dd0a4ce3bb4cf3d2df507bd9a85ab6b7b9c9bbf4","Value":150,"ProgramHash":"e0f1a129f50fc717fe746c5d041d0054874c8609"},{"AssetID":"23d4d5aeb154126332cc9aa5dd0a4ce3bb4cf3d2df507bd9a85ab6b7b9c9bbf4","Value":200,"ProgramHash":"e0f1a129f50fc717fe746c5d041d0054874c8609"}]}]}`
+	//使用p256r1方法加密时使用一个随机数，因此每次加密出来的数据会不太一样
+	//const expect = `800001001335353737303036373931393437373739343130021da2fcb9a6487c7df825dd7324cacc4ac5b5c4d0e22d82594bc5365e0b7b9dcc00001da2fcb9a6487c7df825dd7324cacc4ac5b5c4d0e22d82594bc5365e0b7b9dcc010003f4bbc9b9b7b65aa8d97b50dfd2f34cbbe34c0adda59acc32631254b1aed5d4230003164e02000000c9b43543291886dfa20961bdea57cf0f0f945dcdf4bbc9b9b7b65aa8d97b50dfd2f34cbbe34c0adda59acc32631254b1aed5d42300e721a2040000009962cf555c2b9b0a142cfcec1787b1b534863096f4bbc9b9b7b65aa8d97b50dfd2f34cbbe34c0adda59acc32631254b1aed5d42300f2052a01000000e0f1a129f50fc717fe746c5d041d0054874c86090141400ddf61d9123873b71c8a197c78c8eabb151a8bce709bd42356598f79d3bcfb0b866c42652156e1bfd0c15f536ecf7d5ed31c24ad9f5d9df78b1dfec8483b3c222321036f9c4bc2c75334da4c145848769c6128dd7edafa8059f28da5feb60e3d17e3dfac`
+
+	_, err := SignRawTransaction(prvkey, input)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
